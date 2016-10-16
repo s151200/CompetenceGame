3e6a9adb9351da587ded2395e37f9f7daace5776
Sindri <sindriwest@gmail.com>
2016-10-16 14:20:16 +0200

    New scene, camera script and player script modified. Arrow keys working now.

---
+++ CGame/Assets/Scripts/Camera/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 cameraPosition;
+	public float offsetY = 4f;
+	public float offsetZ = 9f;
+	Vector3 playerPos;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag(TagConstants.PLAYER);
+	}
+
+	void LateUpdate() {
+		playerPos = player.transform.position;
+		transform.position = new Vector3(playerPos.x, playerPos.y + offsetY, playerPos.z - offsetZ);
+	}
+}
---
+++ CGame/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public NavMeshAgent agent;
+	public bool hatOn;
+	public bool clockOn;
+	public float speed;
+
+	void Start() {
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.position += new Vector3(0,0,1) * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.position += new Vector3(0,0,-1) * speed * Time.deltaTime;
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if ( other.transform.tag == TagConstants.CLOCK ) {
+			clockOn = true;
+		}
+		else if ( other.transform.tag == TagConstants.HAT ) {
+			hatOn = true;
+		}
+	}
+}
