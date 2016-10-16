bb94d1a8c9f17f1b4c5c74bc800a0b77b2e8da79
Laura <lauryxky@hotmail.com>
2016-10-16 16:53:08 +0200

    timer added. Worker1 prefab changed so it patrols.

---
+++ CGame/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	private int timeLeft;
+	private int startingTime;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> (); 
+		startingTime = 60;
+		timeLeft = startingTime;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate() {
+		timeLeft = startingTime - (int)Time.timeSinceLevelLoad;
+		text.text = "00:" + timeLeft;
+		if (timeLeft < 10) {
+			text.color = Color.red;
+			text.text = "00:0" + timeLeft;
+		}
+
+		if ( timeLeft == 0 ) {
+			// TODO load game over screen
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+	/*
+	public void AddExtraTime(int extraTime) {
+		startingTime += extraTime;
+	}
+	*/
+}
+
