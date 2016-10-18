ed00fe836e85050604273350a48b6add43498ea5
Laura <lauryxky@hotmail.com>
2016-10-18 16:04:25 +0200

    Level design modifications and player added with animations

--- CGame/Assets/Scripts/Player/Player.cs
+++ CGame/Assets/Scripts/Player/Player.cs
@@ -9,24 +9,43 @@ public class Player : MonoBehaviour {
 	public float turnSmoothing = 3f;
 	float rotateAroundZ;
 	float rotateDegrees = 6f;
+	private Animator animator;
+	private bool isMoving;
 
 	void Start() {
 		agent = GetComponent<NavMeshAgent>();
 		agent.speed = speed;
+		animator = GetComponentInChildren<Animator>();
+		animator.SetBool("NonCombat", true);
+		animator.SetBool("Idling", true);
 	}
 
 	// FixedUpdate is for physics based objects
 	void FixedUpdate() {
+		
 		if ( Input.GetKey(KeyCode.UpArrow) ) {
 			// transform.forward is the forward direction in LOCAL SPACE and then the agent goes forward relative to the agent's angle.
 			// Note: On the other hand Vector3.forward is in WORLD SPACE
 			transform.position += transform.forward * agent.speed * Time.deltaTime;
+			isMoving = true;
+			//Debug.Log("Up arrow");
 		}
 		if ( Input.GetKey(KeyCode.DownArrow) ) {
 			transform.position -= transform.forward * agent.speed * Time.deltaTime;
+			isMoving = true;
+			//Debug.Log("Down arrow");
+		}
+		else if ( Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) ) {
+			animator.SetBool("Idling", true);//stop moving
+			isMoving = false;
+		}
+			
+		RotatePlayer();
+
+		if ( isMoving ) {
+			animator.SetBool("Idling", false);
 		}
 
-		RotatePlayer();
 	}
 
 	// Rotates target around the z-axis
